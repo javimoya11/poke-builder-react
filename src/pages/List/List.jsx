a0f554dd6f0052cbc9a3d94ebc6f746fc8a58750
Javi Moya <javimoya11@gmail.com>
2025-06-12 12:41:21 +0200

    add load next gen button

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -3,19 +3,25 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPokemonList from "../../hooks/fetchPokemonList";
 import fetchTypesList from "../../hooks/fetchTypesList";
 import Pokemon from "../../components/Pokemon/Pokemon";
+import pokedexNumbers from "./pokedexNumbers.json";
 import "./List.css";
 
 function List() {
   const [pokemonSearch, setPokemonSearch] = useState("");
-  const pokeResults = useQuery({ queryKey: ["list"], queryFn: fetchPokemonList });
-  const typeResults = useQuery({ queryKey: ["type-list"], queryFn: fetchTypesList });
+  const [genNumber, setGenNumber] = useState(pokedexNumbers[0]);
+  const pokeResults = useQuery({
+    queryKey: ["list"],
+    queryFn: fetchPokemonList,
+  });
+  const typeResults = useQuery({
+    queryKey: ["type-list"],
+    queryFn: fetchTypesList,
+  });
   if (pokeResults.isLoading || typeResults.isLoading) {
-    return (
-        <h1 >Loading...</h1>
-    );
+    return <h1>Loading...</h1>;
   }
   let pokemons = pokeResults?.data ?? [];
-  let pokemonsLength = 151;
+  let pokemonsLength = genNumber || pokedexNumbers[pokedexNumbers.length - 1];
   if (pokemonSearch.length) {
     pokemons = pokemons.filter((poke) => {
       return poke.name.includes(pokemonSearch.trim());
@@ -46,7 +52,7 @@ function List() {
           e.preventDefault();
           const formData = new FormData(e.target);
           const pokemon = formData.get("pokemon-search") ?? "";
-          setPokemonSearch(pokemon);
+          setPokemonSearch(pokemon.replace(' ', '-'));
         }}
       >
         <div className="search-container">
@@ -63,6 +69,20 @@ function List() {
       <div className="pokemon-list">
         {!list.length ? <h1>No Pokémon Found</h1> : list}
       </div>
+      {!pokemonSearch.length && list.length < pokedexNumbers[pokedexNumbers.length - 1] ? (
+        <button
+          className="load-gen-button"
+          onClick={(e) => {
+            e.preventDefault();
+            const number = pokedexNumbers.find((element) => element > pokemonsLength);
+            setGenNumber(number);
+          }}
+        >
+          Load next gen
+        </button>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
