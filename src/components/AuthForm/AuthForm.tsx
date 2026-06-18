c5eff991c5f104ad4264c9c09cb3c664fae145b3
Javi Moya <javimoya11@gmail.com>
2026-06-19 01:28:51 +0200

    style sign in form

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -10,19 +10,26 @@ interface IAuthForm {
 }
 export const AuthForm = ({ isOpen, onClose }: IAuthForm) => {
   const [emailValue, setEmailValue] = useState<string | null>(null);
+  const [passValue, setPassValue] = useState<string | null>(null);
   const [errorValue, setErrorValue] = useState<AuthError | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [formMode, setFormMode] = useState<'signIn' | 'signUp'>('signIn');
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <form
-        id="sign-in"
+        id={formMode === 'signIn' ? 'sign-in' : 'sign-up'}
         className="auth-form"
         onSubmit={async (e) => {
           e.preventDefault();
           setErrorValue(null);
           setLoading(true);
+          let error: AuthError | null = null;
           if (!emailValue) return;
-          const { error } = await sendMagicLink(emailValue.trim());
+          if (formMode === 'signUp') {
+            error = (await sendMagicLink(emailValue.trim())).error;
+          } else {
+            //
+          }
           if (error) {
             setErrorValue(error);
             return;
@@ -30,8 +37,16 @@ export const AuthForm = ({ isOpen, onClose }: IAuthForm) => {
           setLoading(false);
         }}
       >
+        <nav>
+          <button type="button" onClick={() => setFormMode('signIn')}>
+            Sign In
+          </button>
+          <button type="button" onClick={() => setFormMode('signUp')}>
+            Sign Up
+          </button>
+        </nav>
         <label htmlFor="email">
-          Enter your email
+          Email
           <input
             type="email"
             name="email"
@@ -43,8 +58,44 @@ export const AuthForm = ({ isOpen, onClose }: IAuthForm) => {
             }}
           />
         </label>
-        <button type="submit" form="sign-in" disabled={loading}>
-          Send
+        <label htmlFor="pass">
+          Password
+          <input
+            type="password"
+            name="pass"
+            id="pass"
+            autoComplete="new-password"
+            required
+            onChange={(e) => {
+              setPassValue(e.target.value);
+            }}
+          />
+        </label>
+        {formMode === 'signUp' && (
+          <label htmlFor="pass-confirm">
+            Confirm Password
+            <input
+              type="password"
+              name="pass-confirm"
+              id="pass-confirm"
+              autoComplete="new-password webauthn"
+              required
+              onChange={(e) => {
+                setPassValue(e.target.value);
+              }}
+            />
+          </label>
+        )}
+        <button
+          type="submit"
+          form={formMode === 'signUp' ? 'sign-in' : 'sign-up'}
+          disabled={loading}
+        >
+          {loading ? (
+            <span className="button-spinner" aria-label="Loading" />
+          ) : (
+            'Send'
+          )}
         </button>
         {errorValue ? <div>ERROR</div> : null}
       </form>
